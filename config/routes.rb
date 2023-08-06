cbba46c4c9fdd8859efa3f042bc018905a415260
Anthony Vazhapilly <anthonyvazha@gmail.com>
2023-08-05 20:55:28 -0500

    Completed Frontend design based on wireframe specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'pages/home'
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"# this line is the same as the next line
+  # get '/', to:  'pages#home' # =>  syntax: 'controller_name_here#action_name_here'
 
-  get '/', to:  'pages#home' # =>  syntax: 'controller_name_here#action_name_here'
 end
+
+
+
+# Format: in Terminal Comands to genrate new cotrollers
+
+# rails generate controller <controller_name> <action1> <action2> ...
+
+# Command:
+
+# rails generate controller pages home
+
+#Theory:  Although it's tempting to use generators as much as possible, it's critical you first understand the order of operations between the Client's "request" and the server's "response," specifically from the point at which the request lands on the server:
+
+# Router (config/routes.rb file) tells server what to do with request
+# Controller invokes a specific "action" (function) on the request
+# View (the response template being rendered by the Controller)
